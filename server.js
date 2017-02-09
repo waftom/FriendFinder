6b560a7fe3cae2c30b9b6b51f7951480393d175a
waftom <waftom@gmail.com>
2017-02-09 17:52:42 -0600

    Friend Finder Final 1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var port = 3000;
+var port = 80;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
